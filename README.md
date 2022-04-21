f4d980a19b910c1eb4f3d3545b61074ab796e9f1
Tetsuaki Baba <baba@bagpipe.local>
2022-04-21 09:32:51 +0900

    移動手段追加とエラーチェック追加

--- README.md
+++ README.md
@@ -4,12 +4,14 @@
 <img src="Apr-21-2022 03-38-41.gif">
 
 ## はじめる
+ ```
+git clone https://github.com/TetsuakiBaba/labable.git
+cd labable
+echo '<?php $url = "ここにはslackのカスタムインテグレーションから取得したIncoming Webhook URLを記入する"; ?>' > slack_url.php
+php -S locahost:8000
+```
 
-    git clone https://github.com/TetsuakiBaba/labable.git
-    echo '<?php $url = "ここにはslackのカスタムインテグレーションから取得したIncoming Webhook URLを記入する"; ?>' > slack_url.php
-    php -S locahost:8000
-
-ブラウザから localhost:8000 で閲覧してください。送信したいチャンネル合わせてindex.htmlのチャンネル欄の値を変更してください。
+ブラウザから localhost:8000 で閲覧してください。送信したいチャンネル合わせてindex.htmlのチャンネル欄の値を変更してください。phpコマンドがない場合はmacはbrew等でインストールしてあげてください。
 
 webhook URLの取得については https://slack.com/intl/ja-jp/help/articles/115005265063-Slack-%E3%81%A7%E3%81%AE-Incoming-Webhook-%E3%81%AE%E5%88%A9%E7%94%A8 を参照してください。
 
--- index.js
+++ index.js
@@ -14,9 +14,16 @@ function checkin(dom) {
         return;
     }
 
+    if (!document.querySelector('#agree_checkbox').checked) {
+        alert("注意事項に同意してください");
+        return;
+    }
+
+
     const icon = document.querySelector('#icon').value;
     const channel = document.querySelector('#channel').value;
     const message = document.querySelector('#message').value;
+    const how_to_move = document.querySelector('#select_how_to_move').options[document.querySelector('#select_how_to_move').selectedIndex].innerHTML;
 
     if (37.5 <= temp) {
         alert("無理をせずお家で休みましょう");
@@ -27,7 +34,7 @@ function checkin(dom) {
         name: name,
         channel: channel,
         icon_emoji: icon,
-        text: `*IN @ ${temp}* ${message}`
+        text: `*IN @ ${temp} [${how_to_move}]* ${message}`
     };
     const method = "POST";
     const body = JSON.stringify(obj);
@@ -49,16 +56,24 @@ function checkin(dom) {
     }, 1000);
 }
 function checkout(dom) {
+    if (!document.querySelector('#agree_checkbox').checked) {
+        alert("注意事項に同意してください");
+        return;
+    }
+
+
     const name = document.querySelector('#name').value;
     const temp = document.querySelector('#temp').value;
     const icon = document.querySelector('#icon').value;
     const channel = document.querySelector('#channel').value;
     const message = document.querySelector('#message').value;
+    const how_to_move = document.querySelector('#select_how_to_move').options[document.querySelector('#select_how_to_move').selectedIndex].innerHTML;
+
     const obj = {
         name: name,
         channel: channel,
         icon_emoji: icon,
-        text: `*OUT* ${message}`
+        text: `*OUT [${how_to_move}]* ${message}`
     };
     const method = "POST";
     const body = JSON.stringify(obj);
@@ -70,9 +85,14 @@ function checkout(dom) {
         .then(response => response.json()) // 帰ってきた値をjsonにして次に渡す
         .then(res => {
             console.log(res);
+            showToast(res.message);
         })
         .catch(console.error);
 
+    dom.disabled = true;
+    setTimeout(function () {
+        dom.disabled = false;
+    }, 1000);
 }
 
 function saveText(obj) {
@@ -81,6 +101,22 @@ function saveText(obj) {
 window.onload = function () {
     document.querySelector('#name').value = localStorage.getItem('labable_name');
     document.querySelector('#temp').value = localStorage.getItem('labable_temp');
+
+    let num_selected = localStorage.getItem('labable_select_how_to_move');
+    if (!num_selected) {
+        num_selected = 0;
+    }
+    for (let i = 0; i < document.querySelector('#select_how_to_move').options.length; i++) {
+
+        if (num_selected == i) {
+            document.querySelector('#select_how_to_move').options[i].selected = true;
+        }
+        else {
+            document.querySelector('#select_how_to_move').options[i].selected = false;
+        }
+    }
+
+
 
     if (localStorage.getItem('labable_channel')) {
         document.querySelector('#channel').value = localStorage.getItem('labable_channel');
